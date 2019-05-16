736cf6e7d95d9d3f5827a188fbdf9a9623f414e0
Theo Manton <theo333@gmail.com>
2019-05-16 13:38:43 -0400

    fix port to PORT

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
